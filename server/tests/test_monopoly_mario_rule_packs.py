8e03ea284f38e907e36308ae1fa0c4dba4944247
boo15mario <bala.alek@gmail.com>
2026-02-26 09:47:26 -0600

    Add failing Mario module card capability tests

--- server/tests/test_monopoly_mario_rule_packs.py
+++ server/tests/test_monopoly_mario_rule_packs.py
@@ -11,3 +11,11 @@ def test_mario_pack_exposes_pass_go_contract():
     assert mario_movie.PASS_GO_CREDIT_OVERRIDE is None or isinstance(
         mario_movie.PASS_GO_CREDIT_OVERRIDE, int
     )
+
+
+def test_mario_kart_exports_card_id_remap_mapping():
+    assert mario_kart.CARD_ID_REMAPS[("chance", "bank_dividend_50")] == "advance_to_go"
+
+
+def test_mario_movie_exports_card_cash_override_mapping():
+    assert mario_movie.CARD_CASH_OVERRIDES["bank_dividend_50"] == 120
